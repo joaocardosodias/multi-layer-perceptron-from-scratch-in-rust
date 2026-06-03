e34303e8238992219f6ff1d9bad4843b649d6e68
Joao Cardoso Dias <joao.dias@sou.inteli.edu.br>
2026-06-03 14:41:08 -0300

    feat(linalg): add vector operations and matrix multiplication

--- src/linalg.rs
+++ src/linalg.rs
@@ -1,12 +1,40 @@
-pub fn mat_vec_mul(matrix: &[Vec<f64>], in_vector: &[f64]) -> Vec<f64> {
+pub fn mul_matrix_vec(matrix: &[Vec<f64>], in_vector: &[f64]) -> Vec<f64> {
     let mut out_vector = Vec::new();
     for i in 0..matrix.len() {
-        let mut soma = 0.0;
+        let mut sum = 0.0;
         for j in 0..matrix[i].len() {
-            soma += matrix[i][j] * in_vector[j];
+            sum += matrix[i][j] * in_vector[j];
         }
-        out_vector.push(soma);
-        soma = 0.0;
+        out_vector.push(sum);
+        sum = 0.0;
     }
     out_vector
+}
+pub fn add_vec_vec(vec1: Vec<f64>, vec2: Vec<f64>) -> Vec<f64> {
+    let mut out_vector = Vec::new();
+    for i in 0..vec1.len() {
+        out_vector.push(vec1[i] + vec2[i]);
+    }
+    out_vector
+}
+pub fn mul_vec_vec(vec1: Vec<f64>, vec2: Vec<f64>) -> Vec<f64> {
+    let mut out_vector = Vec::new();
+    for i in 0..vec1.len() {
+        out_vector.push(vec1[i] * vec2[i]);
+    }
+    out_vector
+}
+pub fn mul_matrix_matrix(matrix1: &[Vec<f64>], matrix2: &[Vec<f64>]) -> Vec<Vec<f64>> {
+    let m = matrix1.len();
+    let n = matrix2[0].len();
+    let k = matrix2.len();
+    let mut c = vec![vec![0.0; n]; m];
+    for i in 0..m {
+        for j in 0..n {
+            for p in 0..k {
+                c[i][j] +=matrix1[i][p] * matrix2[p][j];
+            }
+        }
+    }
+    c
 }
